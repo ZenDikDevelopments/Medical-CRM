a2dc8d77a7dba0541d8105e24c8e0a62fd56c51f
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-07 19:56:29 +0300

    Initialize project

---
+++ src/test/java/com/example/medicalcrm/MedicalCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
