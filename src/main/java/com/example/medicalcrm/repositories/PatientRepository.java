3f9bdd9204dce1837f45692996921e3d5cef3142
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-19 12:16:51 +0300

    Added annotations

--- src/main/java/com/example/medicalcrm/repositories/PatientRepository.java
+++ src/main/java/com/example/medicalcrm/repositories/PatientRepository.java
@@ -2,7 +2,9 @@ package com.example.medicalcrm.repositories;
 
 import com.example.medicalcrm.models.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
 }
--- src/main/java/com/example/medicalcrm/repositories/ProcedureRepository.java
+++ src/main/java/com/example/medicalcrm/repositories/ProcedureRepository.java
@@ -2,6 +2,8 @@ package com.example.medicalcrm.repositories;
 
 import com.example.medicalcrm.models.entities.Procedure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProcedureRepository extends JpaRepository<Procedure, Long> {
 }
